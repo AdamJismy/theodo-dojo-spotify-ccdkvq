0f4f43607bf727d71c64cae3d8a524906beab3e9
AdamJismy <131191254+AdamJismy@users.noreply.github.com>
2023-04-18 19:15:41 +0200

    Blind Test

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,10 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Bienvenue sur le blind test</h1>
+        <h1 className="App-title">Blind Test</h1>
       </header>
       <div className="App-images">
-        <p>Il va falloir modifier le code pour faire un vrai blind test ADAM !</p>
+        <p>Il va falloir modifier le code pour faire un vrai blind test!</p>
       </div>
       <div className="App-buttons"></div>
     </div>
